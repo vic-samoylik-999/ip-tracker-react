d88eeeeb6083f216a4b5a689a138d9ead2e9997d
Vic Samoylik <v.samojlik@gmail.com>
2023-12-19 21:39:23 +0300

    feat: variables, basic layout styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 function App() {
-  return <>hello react</>;
+  return (
+    <>
+      <h1>IP Address Tracker</h1>
+      <h3>ip address</h3>
+      <h2>192.212.174.101</h2>
+    </>
+  );
 }
 
 export default App;
