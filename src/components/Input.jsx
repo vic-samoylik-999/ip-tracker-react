a19f975fbf476d0ca9a505514627656fdeda5aa3
Vic Samoylik <v.samojlik@gmail.com>
2023-12-22 23:35:39 +0300

    feat: basic styling for input component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -5,7 +5,11 @@ import arrowIcon from '../assets/icons/icon-arrow.svg';
 function Input() {
   return (
     <section className="input">
-      <input className="input__input" type="text" />
+      <input
+        className="input__input"
+        type="text"
+        placeholder="Search for any IP address or domain"
+      />
       <button className="input__button">
         <img src={arrowIcon} alt="Arrow Icon" />
       </button>
